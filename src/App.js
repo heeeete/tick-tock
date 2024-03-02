106201aa038a8054c42d98eda2b52e2bd65c9bd6
heeeete <mkoui@naver.com>
2024-03-02 18:02:01 +0900

    feat : 여러 나라 표준 시간 추가

--- src/App.js
+++ src/App.js
@@ -1,208 +1,292 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import moment from "moment-timezone";
 
 const AppContainer = styled.div`
-  display: flex;
-  width: 100dvw;
-  height: 100dvh;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	color: white;
+
+	width: 100dvw;
+	height: 100dvh;
+	background: radial-gradient(
+		circle at center,
+		grey,
+		black
+			${(props) => {
+				if (props.$hours >= 18) {
+					return `${120 - props.$hours * 3}%`;
+				} else if (props.$hours < 6) {
+					return `20%`;
+				} else {
+					return `150%`;
+				}
+			}}
+	);
 `;
 
 const Clock = styled.div`
-  width: 40dvw;
-  height: 40dvw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	width: 40dvw;
+	height: 40dvw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: white;
+	/* background-color: blue; */
 `;
 
 const HourHand = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  width: ${(props) => props.$clocksize.height / 17}px;
-  left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
-  bottom: 50%;
-  transform-origin: bottom;
-  transform: rotateZ(${(props) => props.$hours * 30 + props.$minutes / 2}deg);
-  font-size: ${(props) => props.$clocksize.height / 20}px;
-  transition: transform 1s ease;
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	width: ${(props) => props.$clocksize.height / 17}px;
+	left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
+	bottom: 50%;
+	transform-origin: bottom;
+	transform: rotateZ(${(props) => props.$hours * 30 + props.$minutes / 2}deg);
+	font-size: ${(props) => props.$clocksize.height / 20}px;
+	transition: transform 1s ease;
 
-  /* background-color: blue; */
-  & > * {
-    /* margin-block: ${(props) => props.$clocksize.height / 1000}px; */
-  }
+	/* background-color: blue; */
+	& > * {
+		/* margin-block: ${(props) => props.$clocksize.height / 1000}px; */
+	}
 `;
 const MinuteHand = styled.div`
-  text-align: center;
-  display: flex;
+	text-align: center;
+	display: flex;
 
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
-  bottom: 50%;
-  width: ${(props) => props.$clocksize.height / 17}px;
-  transform-origin: bottom;
-  font-size: ${(props) => props.$clocksize.height / 20}px;
-  transform: rotateZ(${(props) => props.$minutes * 6}deg);
-  transition: transform 1s ease;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
+	bottom: 50%;
+	width: ${(props) => props.$clocksize.height / 17}px;
+	transform-origin: bottom;
+	font-size: ${(props) => props.$clocksize.height / 20}px;
+	transform: rotateZ(${(props) => props.$minutes * 6}deg);
+	transition: transform 1s ease;
 
-  /* background-color: green; */
-  & > * {
-    /* margin-block: ${(props) => props.$clocksize.height / 1000}px; */
-  }
+	/* background-color: green; */
+	& > * {
+		/* margin-block: ${(props) => props.$clocksize.height / 1000}px; */
+	}
 `;
 const SecondHand = styled.div`
-  text-align: center;
-  display: flex;
+	text-align: center;
+	display: flex;
 
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  width: ${(props) => props.$clocksize.height / 17}px;
-  left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
-  bottom: 50%;
-  transform-origin: bottom;
-  font-size: ${(props) => props.$clocksize.height / 40}px;
-  transform: rotateZ(${(props) => props.$seconds * 6}deg);
-  transition: transform 1s ease;
-  /* background-color: purple; */
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	width: ${(props) => props.$clocksize.height / 17}px;
+	left: calc(50% - ${(props) => props.$clocksize.height / 18 / 2}px);
+	bottom: 50%;
+	transform-origin: bottom;
+	font-size: ${(props) => props.$clocksize.height / 40}px;
+	transform: rotateZ(${(props) => props.$seconds * 6}deg);
+	transition: transform 1s ease;
+	/* background-color: purple; */
 
-  & > * {
-    margin-block: ${(props) => props.$clocksize.height / 70}px;
-  }
+	& > * {
+		margin-block: ${(props) => props.$clocksize.height / 70}px;
+	}
 `;
 
 const Origin = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 13.3%;
-  height: 13.3%;
-  background-color: white;
-  z-index: 1;
-  border-radius: 50%;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 13.3%;
+	height: 13.3%;
+	z-index: 1;
+	border-radius: 50%;
+	font-size: ${(props) => props.$clocksize.height / 20}px;
+`;
+
+const Date = styled.div`
+	position: absolute;
+	bottom: 0;
+	left: 50%;
+	transform: translate(-50%);
+`;
+
+const CurCountry = styled.div`
+	position: absolute;
+	bottom: 20px;
+	left: 50%;
+	transform: translate(-50%);
+`;
+
+const CountryConatiner = styled.nav`
+	overflow-y: auto;
+	width: auto;
+
+	/* Chrome */
+	&::-webkit-scrollbar {
+		display: none;
+	}
+	-ms-overflow-style: none; /* IE and Edge */
+	scrollbar-width: none; /* Firefox */
+`;
+
+const Country = styled.button`
+	background-color: transparent;
+	width: 100%;
+	border: none;
+	display: flex;
+	padding: 10px 0;
+	color: white;
+	transition: 0.3s;
+	&:hover > span {
+		background-color: rgba(20, 20, 20, 1);
+	}
+`;
+
+const CountrySpan = styled.span`
+	padding: 20px 20px 20px 0;
+	transition: 0.3s;
+	border-top-right-radius: 10px;
+	border-bottom-right-radius: 10px;
 `;
 
 const HoursHandText = styled.div`
-  transform: rotateZ(-${(props) => props.$hours * 30 + props.$minutes / 2}deg);
+	transform: rotateZ(-${(props) => props.$hours * 30 + props.$minutes / 2}deg);
 `;
 const MinutesHandText = styled.div`
-  transform: rotateZ(-${(props) => props.$minutes * 6}deg);
+	transform: rotateZ(-${(props) => props.$minutes * 6}deg);
 `;
 const SecondsHandText = styled.div`
-  transform: rotateZ(-${(props) => props.$seconds * 6}deg);
+	transform: rotateZ(-${(props) => props.$seconds * 6}deg);
 `;
 
 function App() {
-  const [date, setDate] = useState(new Date());
-  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  const [hoursArr, setHoursArr] = useState([]);
-  const clockRef = useRef(null);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  // let minutes = 22;
-  // let seconds = 52;
+	const [country, SetCountry] = useState("Asia/Seoul");
+	const [date, setDate] = useState(moment().tz(country));
+	const [clockSize, setClockSize] = useState({ width: 0, height: 0 });
+	const clockRef = useRef(null);
+	let countryArr = moment.tz.names();
+	let hours = date.hours();
+	let minutes = date.minutes();
+	let seconds = date.seconds();
 
-  // let seconds = 0;
+	useEffect(() => {
+		const timerId = setInterval(() => {
+			setDate(moment().tz(country));
+		}, 100);
 
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
+		return () => clearInterval(timerId);
+	}, [country]);
 
-    return () => clearInterval(timerId);
-  }, []);
+	useEffect(() => {
+		const clockReSize = () => {
+			if (clockRef.current) {
+				setClockSize({
+					width: clockRef.current.offsetWidth,
+					height: clockRef.current.offsetHeight,
+				});
+			}
+		};
+		clockReSize();
+		window.addEventListener("resize", clockReSize);
+		return () => window.removeEventListener("resize", clockReSize);
+	}, []);
 
-  useEffect(() => {
-    const clockReSize = () => {
-      if (clockRef.current) {
-        setDimensions({
-          width: clockRef.current.offsetWidth,
-          height: clockRef.current.offsetHeight,
-        });
-      }
-    };
-    clockReSize();
-    window.addEventListener("resize", clockReSize);
-    return () => window.removeEventListener("resize", clockReSize);
-  }, []);
+	const onClickCountry = (e) => {
+		SetCountry(e.target.innerText);
+	};
 
-  return (
-    <AppContainer>
-      <Clock ref={clockRef}>
-        <Origin>PM</Origin>
-        <HourHand $hours={hours} $minutes={minutes} $clocksize={dimensions}>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-          <HoursHandText $hours={hours} $minutes={minutes}>
-            {hours}
-          </HoursHandText>
-        </HourHand>
-        <MinuteHand $minutes={minutes} $clocksize={dimensions}>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-          <MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
-        </MinuteHand>
-        <SecondHand $seconds={seconds} $clocksize={dimensions}>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-          <SecondsHandText $clocksize={dimensions} $seconds={seconds}>
-            {seconds}
-          </SecondsHandText>
-        </SecondHand>
-      </Clock>
-    </AppContainer>
-  );
+	return (
+		<>
+			<AppContainer $hours={hours}>
+				<CountryConatiner>
+					{countryArr.map((e, idx) => (
+						<Country key={idx} onClick={onClickCountry}>
+							<CountrySpan>{e}</CountrySpan>
+						</Country>
+					))}
+				</CountryConatiner>
+				<Clock ref={clockRef}>
+					<Origin $clocksize={clockSize}>
+						{hours >= 12 && hours < 24 ? "PM" : "AM"}
+					</Origin>
+					<HourHand $hours={hours} $minutes={minutes} $clocksize={clockSize}>
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>{" "}
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>
+						<HoursHandText $hours={hours} $minutes={minutes}>
+							{hours}
+						</HoursHandText>
+						<div>&nbsp;</div>
+					</HourHand>
+					<MinuteHand $minutes={minutes} $clocksize={clockSize}>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>{" "}
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<MinutesHandText $minutes={minutes}>{minutes}</MinutesHandText>
+						<div>&nbsp;</div>
+					</MinuteHand>
+					<SecondHand $seconds={seconds} $clocksize={clockSize}>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>{" "}
+						<SecondsHandText $clocksize={clockSize} $seconds={seconds}>
+							{seconds}
+						</SecondsHandText>
+						<div>&nbsp;</div>
+					</SecondHand>
+				</Clock>
+				<CurCountry>{country}</CurCountry>
+				<Date>{date.format("YYYY-MM-DD")}</Date>
+			</AppContainer>
+		</>
+	);
 }
 
 export default App;
